d3ad6b69f38e2469f32f3b2c3f83e35688bdf73d
Jay <chunsaa@gmail.com>
2020-04-02 04:49:53 -0400

    add cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 //     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 //     next();
 // });
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
