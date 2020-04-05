98d882dee0c471f72c81c017f0d6c807a6f53ee3
Jay <chunsaa@gmail.com>
2020-04-05 04:08:54 -0400

    cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
 });
-app.use(cors())
+//app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
