737bad76d466b3cd76535d0082c9de48499b8a30
Jay <chunsaa@gmail.com>
2020-04-06 21:07:54 -0400

    add bcrypt for pw hash and implement logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,74 @@
 const express = require('express');
 const router = express.Router();
-let userId;
+const { db } = require('../config');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
   if (req.session.userId) {
     res.json("in")
   } else {
     res.json("out")
-  }  
+  }
 })
 
 router.post('/login', (req, res) => {
-  req.session.userId = 1
-  res.json("in")
+  const values = [req.body.email];
+  const queryString = `
+  SELECT * FROM users
+  WHERE email = $1
+  `;
+  db.query(queryString, values, (error, results) => {
+    if (error) {
+      throw error
+    }
+    if (results.rows.length) {      
+      bcrypt.compare(req.body.password, results.rows[0].password)
+      .then(result => {
+        console.log(result)
+        if (result) {
+          req.session.userId = results.rows[0].id
+          res.json("logged in")
+        }
+        else res.json("wrong pw")
+      })      
+    }
+    else res.json("user not found")
+  })
+})
+
+router.post('/register', (req, res) => {
+  const values = [req.body.email];
+  const queryString = `
+  SELECT * FROM users
+  WHERE email = $1
+  `;
+  db.query(queryString, values, (error, results) => {
+    if (error) {
+      throw error
+    }
+    if (results.rows.length) {
+      res.json("email already registered")
+    }
+    else bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const values = [req.body.firstName, req.body.lastName, hash, req.body.email, req.body.phone];
+        const queryString = `
+        INSERT INTO users(firstName, lastName, password, email, phone)
+        VALUES ($1, $2, $3, $4, $5)
+        `;
+        db.query(queryString, values, (error, results) => {
+          if (error) {
+            throw error
+          }          
+          res.json("user created")
+        })
+      })
+  })
 })
 
 router.post('/logout', (req, res) => {
-  req.session.userId = null
-  res.json("out")
+  req.session.userId = undefined
+  res.json("logged out")
 })
 
 module.exports = router;
