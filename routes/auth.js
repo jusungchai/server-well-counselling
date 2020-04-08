1bce99b9515f9e3f563edaaf40b67959c05c1c3d
Jay <chunsaa@gmail.com>
2020-04-07 22:23:08 -0400

    add reg key check

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const { db } = require('../config');
@@ -21,49 +22,54 @@ router.post('/login', (req, res) => {
     if (error) {
       throw error
     }
-    if (results.rows.length) {      
+    if (results.rows.length) {
       bcrypt.compare(req.body.password, results.rows[0].password)
-      .then(result => {
-        console.log(result)
-        if (result) {
-          req.session.userId = results.rows[0].id
-          res.json("logged in")
-        }
-        else res.json("wrong pw")
-      })      
+        .then(result => {
+          console.log(result)
+          if (result) {
+            req.session.userId = results.rows[0].id
+            res.json("logged in")
+          }
+          else res.json("wrong pw")
+        })
     }
     else res.json("user not found")
   })
 })
 
 router.post('/register', (req, res) => {
-  const values = [req.body.email];
-  const queryString = `
-  SELECT * FROM users
-  WHERE email = $1
-  `;
-  db.query(queryString, values, (error, results) => {
-    if (error) {
-      throw error
-    }
-    if (results.rows.length) {
-      res.json("email already registered")
-    }
-    else bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const values = [req.body.firstName, req.body.lastName, hash, req.body.email, req.body.phone];
-        const queryString = `
+  if (req.body.registrationKey === process.env.REGISTRATION_KEY) {
+    const values = [req.body.email];
+    const queryString = `
+    SELECT * FROM users
+    WHERE email = $1
+    `;
+    db.query(queryString, values, (error, results) => {
+      if (error) {
+        throw error
+      }
+      if (results.rows.length) {
+        res.json("email already registered")
+      }
+      else bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+          const values = [req.body.firstName, req.body.lastName, hash, req.body.email, req.body.phone];
+          const queryString = `
         INSERT INTO users(firstName, lastName, password, email, phone)
         VALUES ($1, $2, $3, $4, $5)
         `;
-        db.query(queryString, values, (error, results) => {
-          if (error) {
-            throw error
-          }          
-          res.json("user created")
+          db.query(queryString, values, (error, results) => {
+            if (error) {
+              throw error
+            }
+            res.json("user created")
+          })
         })
-      })
-  })
+    })
+  }
+  else {
+    res.json("Invalid Registration Key")
+  }
 })
 
 router.post('/logout', (req, res) => {
