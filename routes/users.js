ab12bd395d1071133cecd15631bcdb098177234f
Jay <chunsaa@gmail.com>
2020-04-11 03:40:39 -0400

    add get route for individual profile

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,23 @@ const router = express.Router();
 
 router.get('/profiles', (req, res) => {
   const queryString = `
-  SELECT users.id, firstName, lastName FROM users JOIN profiles ON users.id=profiles.userId
+  SELECT profiles.userId, firstName, lastName, profiles.data -> 'avatarURL' as avatarURL FROM users JOIN profiles ON users.id=profiles.userId
   `;
   db.query(queryString, (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.json(results.rows)
+  })
+})
+
+router.get('/profile', (req, res) => {
+  const values = [req.query.id]
+  const queryString = `
+  SELECT profiles.userId, firstName, lastName, profiles.data FROM users JOIN profiles ON users.id=profiles.userId
+  WHERE profiles.userId = $1
+  `;
+  db.query(queryString, values, (error, results) => {
     if (error) {
       throw error
     }
